e23ed4b39e9a04fa079feba4c648ce7093912e11
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-19 19:45:27 +0530

    [Shivaraj] Implemented printing array using generic class

---
+++ ArrayOfDifferentDataTypes/ArrayOfDifferentDataTypes/ArrayListUsingGenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListOfDifferentDataTypes
+{
+    public class ArrayListUsingGenericClass<T>
+    {
+        public T[] inputArray;
+        public ArrayListUsingGenericClass(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void ToPrint()  //Generic Method
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+    }  
+}
--- ArrayOfDifferentDataTypes/ArrayOfDifferentDataTypes/Program.cs
+++ ArrayOfDifferentDataTypes/ArrayOfDifferentDataTypes/Program.cs
@@ -11,28 +11,31 @@ namespace ArrayListOfDifferentDataTypes
             arrayListOfData.PrintIntArray();
             arrayListOfData.PrintDoubleArray();
             arrayListOfData.PrintCharArray();
-           
-            
-            
             //Intialising Array To print Array List using Generics
             int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
             double[] doubleArray = { 1.2, 1.3, 1.4, 1.5 };
             char[] charArray = { 'A', 'B', 'C', 'D' };
             ArrayListWithGenerics arrayListWithGenerics = new ArrayListWithGenerics();
+
+            //When you use generic class no need of declaring with generic method // Vice Versa
             Console.WriteLine("Using Generic Method: ");
             arrayListWithGenerics.ToprintGeneric<int>(intArray);
             arrayListWithGenerics.ToprintGeneric<double>(doubleArray);
             arrayListWithGenerics.ToprintGeneric<char>(charArray);
-         
-            Console.ReadLine();
+
+            Console.WriteLine("Using Generic Classes: ");
+            new ArrayListUsingGenericClass<int>(intArray).ToPrint();
+            new ArrayListUsingGenericClass<double>(doubleArray).ToPrint();
+            new ArrayListUsingGenericClass<char>(charArray).ToPrint();
         }
     }
     
 }
-//UC-2
-////Print Array of Different Data Types using Generics
+//UC-3
+////Print Array of Different Data Types using Normal Method,Generic Method & Generic Class.
 
 //Result
+//Using Normal Method:
 //1
 //2
 //3
@@ -51,3 +54,41 @@ namespace ArrayListOfDifferentDataTypes
 //C
 //D
 //------------------------------------
+//Using Generic Method:
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//---------------------------------- -
+//1.2
+//1.3
+//1.4
+//1.5
+//---------------------------------- -
+//A
+//B
+//C
+//D
+//-----------------------------------
+//Using Generic Classes:
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//---------------------------------- -
+//1.2
+//1.3
+//1.4
+//1.5
+//---------------------------------- -
+//A
+//B
+//C
+//D
+//-----------------------------------
