7f2df387e7598aa04b53b4d67c8379963f567d59
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-19 18:16:13 +0530

    [Shivaraj] Stored Printing Array Of Different Data Types

--- ArrayOfDifferentDataTypes/ArrayOfDifferentDataTypes/Program.cs
+++ ArrayOfDifferentDataTypes/ArrayOfDifferentDataTypes/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Learninig Problem of Array of different datatypes");
+//Console.WriteLine("Welcome to Learninig Problem of Array of different datatypes");
+namespace ArrayListOfDifferentDataTypes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayListOfDataTypes arrayListOfData = new ArrayListOfDataTypes();
+            arrayListOfData.PrintIntArray();
+            arrayListOfData.PrintDoubleArray();
+            arrayListOfData.PrintCharArray();
+            Console.ReadLine();
+        }
+    }
+    
+}
+//UC-1
+////Print Array of Different Data Types
+
+//Result
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//------------------------------------
+//1.2
+//1.3
+//1.4
+//1.5
+//------------------------------------
+//A
+//B
+//C
+//D
+//------------------------------------
